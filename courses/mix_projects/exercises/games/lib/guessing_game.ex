44971d3b2f9e80f1e5abe3644f1419790524a10c
tuht97 <htinhtu@gmail.com>
2023-12-13 16:41:46 +0700

    finish Games: Guessing Game exercise

---
+++ courses/mix_projects/exercises/games/lib/guessing_game.ex
@@ -0,0 +1,26 @@
+defmodule Games.GuessingGame do
+  def play do
+    rand_numb = Enum.random(1..10)
+    IO.puts("Guess a number between 1 and 10:")
+    guess_loop(rand_numb, 10)
+  end
+
+  defp guess_loop(rand_numb, 0), do: IO.puts("You lose! the answer was #{rand_numb}")
+
+  defp guess_loop(rand_numb, attempts) do
+    guess_numb = IO.gets("Enter your guess:") |> String.trim() |> String.to_integer()
+
+    cond do
+      guess_numb == rand_numb ->
+        IO.puts("Correct!")
+
+      guess_numb < rand_numb ->
+        IO.puts("Too Low!")
+        guess_loop(rand_numb, attempts - 1)
+
+      guess_numb > rand_numb ->
+        IO.puts("Too High!")
+        guess_loop(rand_numb, attempts - 1)
+    end
+  end
+end
